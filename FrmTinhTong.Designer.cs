a50841067627508d959888dc3dfcd4e3b10d9501
huonggiang132 <gianghg301@gmail.com>
2020-05-06 19:22:35 +0700

    cmt

---
+++ FrmTinhTong.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace bt_chuong4
+{
+    partial class FrmTinhTong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txta = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btnTinhTong = new System.Windows.Forms.Button();
+            this.lbTong = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(323, 91);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(132, 22);
+            this.txta.TabIndex = 0;
+            this.txta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txta_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nhập a =";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Nhập b = ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(323, 145);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(132, 22);
+            this.txtb.TabIndex = 3;
+            this.txtb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtb_KeyPress);
+            // 
+            // btnTinhTong
+            // 
+            this.btnTinhTong.Location = new System.Drawing.Point(58, 334);
+            this.btnTinhTong.Name = "btnTinhTong";
+            this.btnTinhTong.Size = new System.Drawing.Size(83, 33);
+            this.btnTinhTong.TabIndex = 4;
+            this.btnTinhTong.Text = "Tổng";
+            this.btnTinhTong.UseVisualStyleBackColor = true;
+            this.btnTinhTong.Click += new System.EventHandler(this.btnTinhTong_Click);
+            // 
+            // lbTong
+            // 
+            this.lbTong.AutoSize = true;
+            this.lbTong.Location = new System.Drawing.Point(232, 213);
+            this.lbTong.Name = "lbTong";
+            this.lbTong.Size = new System.Drawing.Size(0, 17);
+            this.lbTong.TabIndex = 5;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(531, 334);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(81, 33);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.Location = new System.Drawing.Point(289, 334);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(89, 33);
+            this.btnLamLai.TabIndex = 7;
+            this.btnLamLai.Text = "Làm Lại";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // FrmTinhTong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.lbTong);
+            this.Controls.Add(this.btnTinhTong);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txta);
+            this.Name = "FrmTinhTong";
+            this.Text = "Chương trình tính tổng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btnTinhTong;
+        private System.Windows.Forms.Label lbTong;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnLamLai;
+    }
+}
---
+++ FrmTinhTong.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bt_chuong4
+{
+    public partial class FrmTinhTong : Form
+    {
+        public FrmTinhTong()
+        {
+            InitializeComponent();
+        }
+
+        private void txta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8)
+                        || (Convert.ToInt32(e.KeyChar) == 13))
+                e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void txtb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) || (e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8)
+                        || (Convert.ToInt32(e.KeyChar) == 13))
+                e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void btnTinhTong_Click(object sender, EventArgs e)
+        {
+            double tong = 0 ;
+            double i;
+            double a = Convert.ToDouble(txta.Text);
+            double b = Convert.ToDouble(txtb.Text);
+            if (a < b) for (i = a;i <= b; i++)
+                { tong = tong + i; }
+            if ( a > b ) for (i = b;i<=a; i++)
+                { tong = tong + i; }
+            lbTong.Text ="Tong cac so tu" +a+ " den"+b+"="+tong.ToString();
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            txta.Clear();
+            txtb.Clear();
+            txta.Focus();
+        }
+    }
+}
